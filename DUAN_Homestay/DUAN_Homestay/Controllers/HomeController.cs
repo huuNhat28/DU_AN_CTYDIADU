2850a4859f87ce3261e17fcf257123024892410d
huuNhat28 <21t1020555@husc.edu.vn>
2025-02-20 15:14:29 +0700

    nhat

---
+++ DUAN_Homestay/DUAN_Homestay/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using DUAN_Homestay.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DUAN_Homestay.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
