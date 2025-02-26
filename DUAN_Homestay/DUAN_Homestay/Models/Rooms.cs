2cbe1e9ca7ef843db71641dd69bcf35cd3f0b3d4
Hữu Nhật <21t1020555@husc.edu.vn>
2025-02-26 11:25:57 +0700

    Rename rooms.cshtml to Rooms.cshtml

---
+++ DUAN_Homestay/DUAN_Homestay/Models/Rooms.cs
@@ -0,0 +1,14 @@
+﻿namespace DUAN_Homestay.Models
+{
+    public class Rooms
+    {
+        public int RoomID { get; set; }
+        public string RoomNumber { get; set; }
+        public int CategoryID { get; set; }
+        public decimal Price { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
