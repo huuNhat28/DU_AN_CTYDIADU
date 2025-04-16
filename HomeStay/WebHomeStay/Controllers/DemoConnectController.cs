f556cbf8e23e8a389e61a6360568ceb5be2eb9f2
NguyenDuc <duc.diadu@gmail.com>
2025-04-16 15:11:35 +0700

    commit branch ducn

---
+++ HomeStay/WebHomeStay/Controllers/DemoConnectController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebHomeStay.Models;
+
+namespace WebHomeStay.Controllers
+{
+    public class DemoConnectController : Controller
+    {
+        InternWebsiteContext _db = new InternWebsiteContext();
+        public IActionResult Index()
+        {
+            var getBaiViet = _db.BaiViets.ToList();
+            return View();
+        }
+    }
+}
